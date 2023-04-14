077338b08072a19c231d7d4f414bd3a2abc73e24
yong <mail@yongtang.tech>
2023-04-14 07:48:21 -0700

    make the code workable

--- blueteam/ssh-cipher.sh
+++ blueteam/ssh-cipher.sh
@@ -1,18 +1,34 @@
 #! /bin/bash
 
-input='ips.txt' 
+## DATA Processing From a CVS file with AWK
 
+
+
+
+## create a file path for IPs input ##
+file_path='code/test-data/ips.txt'
+full_file_path="$HOME/$file_path"
+
+## check file path ##
+if ! [[ -f "$full_file_path" ]]; then
+   echo "$full_file_path not exit"
+   exit
+fi
+## create a function to connet a ssh server and
+## provide connection result for analysis
+## three "-o" options are set to avoid interactive shell and shorten timeout time. 
+ssh_test () {
+	ssh   -vvv -o PasswordAuthentication=no -o StrictHostKeyChecking=no -o ConnectTimeout=5 "$line" 2>&1
+}
 
 while read -r line
 do
-  output=$(ssh -T -o PasswordAuthentication=no -o StrictHostKeyChecking=no "$line")
-  echo $output 
-  
-#| grep  -q 'cbc';
-   #then 
-	#echo "$line: "
-#fi
-done < "$input"
+  if ssh_test | grep -q 'Connection timed out'; then
+     echo "Connection timed out"
+  elif ssh_test | grep -q 'cbc'; then 
+	echo "$line "
+  fi
+done < "$full_file_path"
 
 
 
